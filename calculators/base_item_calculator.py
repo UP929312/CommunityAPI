83ff82f66378d6b2d6949db3c8a874bbb2438ea3
UP929312 <36114463+UP929312@users.noreply.github.com>
2021-06-03 13:35:25 +0100

    Enchantments now check BIN prices instead of flat constant for each level, much more accurate!

--- calculators/base_item_calculator.py
+++ calculators/base_item_calculator.py
@@ -21,32 +21,34 @@ def calc_stars(item_name, internal_id):
 
 def calculate_item(item, print_prices=False):        
     if item.internal_name in BAZAAR:
-        base_price = BAZAAR[item.internal_name]
+        base_price = int(BAZAAR[item.internal_name])
     if item.internal_name in LOWEST_BIN:
-        base_price = LOWEST_BIN[item.internal_name]
+        base_price = int(LOWEST_BIN[item.internal_name])
     else:
         base_price = prices.get(item.name.upper().replace(" ", "_"), 0)  # The price list uses the item name, not the internal_id.
 
     hot_potato_value, recombobulated_value, star_value, warped_value, enchants_value = (0, 0, 0, 0, 0)
 
     # Hot potato books:
-    hot_potato_value += item.hot_potatos*1_000 if item.hot_potatos <= 10 else (9-item.hot_potatos)*10_000  # 9 because the previous 10 normal books equal 1 Fuming
-    # Recombobulation
+    if item.hot_potatos > 0:
+        if item.hot_potatos <= 10:
+            hot_potato_value += item.hot_potatos*BAZAAR["HOT_POTATO_BOOK"]
+        else:
+            hot_potato_value += 10*BAZAAR["HOT_POTATO_BOOK"]+(10-item.hot_potatos)*BAZAAR["FUMING_POTATO_BOOK"]
+     # Recombobulation
     if item.recombobulated:
-        recombobulated_value = 5_000_000
+        recombobulated_value = int(BAZAAR["RECOMBOBULATOR_3000"])
     # Dungeon items/stars
     if "✪" in item.name:
         star_value = calc_stars(item.name, item.internal_name)
     # Warped aspect of the end
     if ('warped', 1) in item.enchantments:
-        warped_value = 10_000_00 if item.rarity == "epic" else 5_000_000
+        warped_stone_price = LOWEST_BIN.get("AOTE_STONE", 5_000_000)
+        warped_value = warped_stone_price+10_000_000 if item.rarity == "epic" else warped_stone_price+5_000_000
 
     # Enchantments
     for enchantment, level in item.enchantments.items():
-        if level == 6:
-            enchants_value += 500_000
-        elif level >= 7:
-            enchants_value += 1_000_000
+        enchants_value += LOWEST_BIN.get(f"{enchantment.upper()};{level}", 0)
 
     price = sum([base_price, hot_potato_value, recombobulated_value, star_value, warped_value, enchants_value])
 
@@ -57,8 +59,7 @@ def calculate_item(item, print_prices=False):
     #self.reforge = extras.get('modifier', None)
     #=================
     if print_prices:
-        if True:#if price == 0:
-            print("------------")
-            print(f"{item.name.upper().replace(' ', '_')} (x{item.stack_size})")
-            print(f"> {price}, Recom:{recombobulated_value}, ✪: {star_value}, warped? {warped_value}, enchnts: {enchants_value}")
+        print("------------")
+        print(f"{item.name.upper().replace(' ', '_')} (x{item.stack_size})")
+        print(f"> {price}, Recom:{recombobulated_value}, ✪: {star_value}, warped? {warped_value}, enchnts: {enchants_value}")
     return price
--- calculators/main_calculator_handler.py
+++ calculators/main_calculator_handler.py
@@ -4,7 +4,6 @@ from calculators.base_item_calculator import calculate_item
 
 
 def calculate_container(elements, print_prices=False):
-
     total = 0
     for element in elements:            
         if isinstance(element, dict) and 'uuid' in element.keys() and 'active' in element.keys():
@@ -12,6 +11,6 @@ def calculate_container(elements, print_prices=False):
         elif element.internal_name == "ENCHANTED_BOOK":
             price = calculate_enchantment(element)
         else:
-            price = calculate_item(element, print_prices=False)
+            price = calculate_item(element, print_prices)
         total += price
     return total
